5725ac0bf77a934d6a652865e296d1b81898509a
PapeMNdiaye1 <pmomar58@gmail.com>
2026-03-09 00:13:46 +0000

    Ina-111

--- src/main/Pages/Contact.js
+++ src/main/Pages/Contact.js
@@ -13,43 +13,10 @@ function Contact() {
 
       <div class="contact_container_flex">
         <div class="contact_container">
-          <a
-            href="https://api.whatsapp.com/send?phone=221766117508&text="
-            target="_blank"
-          >
-            <div onMouseOver={hoverOnContact} class="contact_cart">
-              Whatsapp
-            </div>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/ina-thiam-b13bb5281/"
-            target="_blank"
-          >
-            <div onMouseOver={hoverOnContact} class="contact_cart">
-              linkedin
-            </div>
-          </a>
-          <a
-            href="https://x.com/inanfthiam?s=21&t=oSME2Spx7YOFp0F-kXd1CQ"
-            target="_blank"
-          >
-            <div onMouseOver={hoverOnContact} class="contact_cart">
-              twitter
-            </div>
-          </a>
-          <a
-            href="https://www.instagram.com/ina_makosi?igsh=MWFwaTh0cjdqZHUwNQ=="
-            target="_blank"
-          >
-            <div onMouseOver={hoverOnContact} class="contact_cart">
-              instagram
-            </div>
-          </a>
-          <a href="mailto:makosiprod@gmail.com?subject=" target="_blank">
-            <div onMouseOver={hoverOnContact} class="contact_cart">
-              mail
-            </div>
-          </a>
+          <div class="Logos"></div>
+          <div class="Logos"></div>
+          <div class="Logos"> </div>
+          <div class="Logos"></div>
         </div>
       </div>
       <div id="tel_gMail">
