f8c6924f126fb104644584351c3213d72dd25f2b
PapeMNdiaye1 <pmomar58@gmail.com>
2026-04-13 14:44:34 +0000

    Formation modif pour Juin 4

--- src/main/Pages/Contact.js
+++ src/main/Pages/Contact.js
@@ -32,7 +32,9 @@ function Contact() {
         </div>
       </div>
       <div id="tel_gMail">
-        Mail: makosiprod@gmail.com <br />
+        Mail: makosiprod@gmail.com / ina.nfthiam@gmail.com /
+        makosi.sports@gmail.com
+        <br />
         Tel: +221 76 611 75 08
       </div>
     </div>
